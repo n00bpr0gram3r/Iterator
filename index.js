87d4ec0134fe6019d27d5eb7272d13589e00a222
n00bpr0gram3r <hnnnnjii@gmail.com>
2022-08-10 17:27:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello');
+console.log('Tutorial on iterator');
+
+function arrayIterator(array) {
+  let nextIndex = 0;
+  //We will return an object
+  return {
+    next: function () {
+      if (nextIndex < array.length) {
+        //We will return below object
+        return {
+          value: array[nextIndex++],
+          done: false,
+        };
+      } else {
+        return {
+          value: 'Array Iteration is Over',
+          done: true,
+        };
+      }
+    },
+  };
+}
+const fruit = ['apple', 'oranges', 'Grapes', 'Banana'];
+const itr = new arrayIterator(fruit);
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
